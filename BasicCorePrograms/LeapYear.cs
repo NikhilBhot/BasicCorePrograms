570d77036d325985f884f35d68015b369270da03
unknown <bhotnikhil25@gmail.com>
2023-03-02 09:42:20 +0530

    [Nikhil]-Leap Year Program

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Leap Year
+    a. I/P -> Year, ensure it is a 4 digit number.
+    b. Logic -> Determine if it is a Leap Year.
+    c. O/P -> Print the year is a Leap Year or not.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class LeapYear
+    {
+       public static void FindLeapYear()
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year.", Year);
+            }
+            else
+                Console.WriteLine("{0} is not a Leap Year.", Year);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -11,8 +11,18 @@
                 a.I / P->The number of times to Flip Coin. Ensure it is a positive integer.
                 b.Logic->Use Random Function to get value between 0 and 1.If < 0.5 then tails or heads
                 c.O / P->Percentage of Head vs Tails
+
+                 FlipCoin.FlipCoinMethod();
             */
-            FlipCoin.FlipCoinMethod();
+
+            /*
+           UC-02-Leap Year
+                a.I / P->Year, ensure it is a 4 digit number.
+                b.Logic->Determine if it is a Leap Year.
+                c.O / P->Print the year is a Leap Year or not.
+            */
+
+            LeapYear.FindLeapYear();
         }
     }
 }
