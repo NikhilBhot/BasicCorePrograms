d5d23e92a0c76afc6dd8d7b02556d22089ac62cf
unknown <bhotnikhil25@gmail.com>
2023-03-02 17:34:32 +0530

    [Nikhil]-Power Of Two

---
+++ BasicCorePrograms/PowerOf2.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Power of 2
+    a. Desc -> This program takes a command-line argument N and prints a table of the
+        powers of 2 that are less than or equal to 2^N.
+    b. I/P -> The Power Value N. Only works if 0 <= N < 31 since 2^31 overflows an int
+    c. Logic -> repeat until i equals N.
+    d. O/P -> Print the year is a Leap Year or not.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class PowerOf2
+    {
+        public static void findPowerOfTwo()
+        {
+            int e;
+            int result;
+
+            for (int i = 0; i < 10; i++)
+            {
+                result = 1;
+                e = i;
+
+                while (e > 0)
+                {
+                    result *= 2;
+                    e--;
+                }
+
+                Console.WriteLine("2 to the " + i + " power is " + result);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -20,9 +20,22 @@
                 a.I / P->Year, ensure it is a 4 digit number.
                 b.Logic->Determine if it is a Leap Year.
                 c.O / P->Print the year is a Leap Year or not.
+
+                 LeapYear.FindLeapYear();
             */
 
-            LeapYear.FindLeapYear();
+            /*
+             * * Power of 2
+                a. Desc -> This program takes a command-line argument N and prints a table of the
+                    powers of 2 that are less than or equal to 2^N.
+                b. I/P -> The Power Value N. Only works if 0 <= N < 31 since 2^31 overflows an int
+                c. Logic -> repeat until i equals N.
+                d. O/P -> Print the year is a Leap Year or not.
+             */
+
+            PowerOf2.findPowerOfTwo();
+
+
         }
     }
 }
